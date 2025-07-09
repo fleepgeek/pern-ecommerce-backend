83fdd5565bcee9f25c682977d973c11feea84cec
fleepgeek <fleepgeek@gmail.com>
2025-07-09 16:19:18 +0100

    update port

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ dotenv.config();
 
 configureCloudinary();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7000;
 const app = express();
 
 // app.use(cors({ credentials: true, origin: "http://localhost:5000" }));
